c3f2710111bf832cc07ac32402d510d7b395b3b0
umutboz <umut.boz@outlook.com>
2023-01-23 15:43:14 +0300

    kotlin list is added theme1 theme2  add example coding

--- src/code_gen_lib_kotlin_list.py
+++ src/code_gen_lib_kotlin_list.py
@@ -28,40 +28,79 @@ xml_extension = ".xml"
 # should be name property in root mustache_folder property directory
 # all mustache files belonging to for this module
 
+# example : com.kocsistem.codegendemo City theme1 -o /Users/***/Desktop/kotlin-list-id
+
 # param1 : package
 # param2 : name
-if len(sys.argv) >= 2:
+# param3 : theme -> theme1 - theme2
+if len(sys.argv) >= 6 and "theme" in sys.argv[3]:
     params = str(sys.argv[0])
     package = str(sys.argv[1])
     name = str(sys.argv[2])
+    theme = str(sys.argv[3])
+    output_param = str(sys.argv[3])
 
     activity_name = name + "Activity"
     activity_layout_name = str(name).lower() + "_activity_layout"
     item_layout_xml = str(name).lower() + "_item_layout"
     adapter_name = name + "Adapter"
+    model = str(name).lower()
 
-    activity_TF = TemplateFile(
-        name="list_activity_mustache",
-        dict={"package_name": package, "activity_name": activity_name,
-              "list_activity_layout_xml_name": activity_layout_name},
-        output_file=activity_name + kotlin_extension
-    )
-    layout_xml_TF = TemplateFile(
-        name="list_activity_layout_xml_mustache",
-        dict={"activity_name": activity_name},
-        output_file=activity_layout_name + xml_extension
-    )
-    adapter_TF = TemplateFile(
-        name="list_adapter_mustache",
-        dict={"adapter_name": adapter_name, "package_name": package, "list_item_xml_name": item_layout_xml},
-        output_file=adapter_name + kotlin_extension
-    )
+    activity_TF = None
+    adapter_TF = None
+    layout_xml_TF = None
+    item_layout_xml_TF = None
 
-    item_layout_xml_TF = TemplateFile(
-        name="list_item_xml_mustache",
-        dict={},
-        output_file=item_layout_xml + xml_extension
-    )
+    if theme == "theme2":
+        # theme2
+        activity_TF = TemplateFile(
+            name="list_activity_theme2_mustache",
+            dict={"package_name": package, "activity_name": activity_name,
+                  "list_activity_layout_xml_name": activity_layout_name,
+                  "adapter_name": adapter_name},
+            output_file=activity_name + kotlin_extension
+        )
+        item_layout_xml = item_layout_xml + "_theme2"
+        adapter_TF = TemplateFile(
+            name="list_adapter_theme2_mustache",
+            dict={"adapter_name": adapter_name, "package_name": package, "list_item_xml_name": item_layout_xml,
+                  "model": model},
+            output_file=adapter_name + kotlin_extension
+        )
+        layout_xml_TF = TemplateFile(
+            name="list_activity_layout_xml_mustache",
+            dict={"activity_name": activity_name, "list_item_xml_name": item_layout_xml},
+            output_file=activity_layout_name + xml_extension
+        )
+        item_layout_xml_TF = TemplateFile(
+            name="list_item_xml_theme2_mustache",
+            dict={"model": model},
+            output_file=item_layout_xml + xml_extension
+        )
+    elif theme == "theme1":
+        activity_TF = TemplateFile(
+            name="list_activity_theme1_mustache",
+            dict={"package_name": package, "activity_name": activity_name,
+                  "list_activity_layout_xml_name": activity_layout_name,
+                  "adapter_name": adapter_name},
+            output_file=activity_name + kotlin_extension
+        )
+        item_layout_xml = item_layout_xml + "_theme1"
+        adapter_TF = TemplateFile(
+            name="list_adapter_theme1_mustache",
+            dict={"adapter_name": adapter_name, "package_name": package, "list_item_xml_name": item_layout_xml},
+            output_file=adapter_name + kotlin_extension
+        )
+        layout_xml_TF = TemplateFile(
+            name="list_activity_layout_xml_mustache",
+            dict={"activity_name": activity_name, "list_item_xml_name": item_layout_xml},
+            output_file=activity_layout_name + xml_extension
+        )
+        item_layout_xml_TF = TemplateFile(
+            name="list_item_xml_theme1_mustache",
+            dict={},
+            output_file=item_layout_xml + xml_extension
+        )
 
     # modules/android-kotlin-list
     listModule = TemplateModule(
@@ -69,17 +108,22 @@ if len(sys.argv) >= 2:
         mustache_folder="modules",
         templates_files=[activity_TF, layout_xml_TF, adapter_TF, item_layout_xml_TF]
     )
-    listModule.outputDirectory = "output"
+    listModule.isGenerateOutPutFullPath = True
+    listModule.outputDirectory = sys.argv[5]
     listModule.isAppendOutputPath = True
     tStreaming = TemplateStreaming(
         template_module=listModule
     )
-    tStreaming.onlineMustache = True
+    tStreaming.onlineMustache = False
     tStreaming.execute()
 else:
     # python main.py com.mypackage.app List
-    Log.e("must be use min. 2 parameters ")
-    Log.i("or u can use params --info")
-
-#https://raw.githubusercontent.com/umutboz/code-gen-lib/master/modules/android-kotlin-list/list_item_xml_mustache
+    Log.e("must be use min. 4 parameters ")
+    Log.i("First param is :  package id")
+    Log.i("Second param is :  Naming")
+    Log.i("third param :  theme1 or theme2")
+    Log.i("fifth param is with -o :  output path folder")
+    if len(sys.argv) >= 4:
+        if sys.argv[4] != "-o":
+            Log.e("must be use -o param due to output path")
 
